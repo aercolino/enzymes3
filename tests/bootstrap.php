e6edfc45ef6fa9e27e986987e0da9f79bce2559c
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2017-04-11 18:42:44 +0200

    Fix bootstrap for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,25 +1,28 @@
 <?php
 
+global $_project_dir;
+$_project_dir = dirname(__DIR__);
+require_once "$_project_dir/vendor/autoload.php";
+
 $_tests_dir = getenv('WP_TESTS_DIR');
 if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
-require_once $_tests_dir . '/includes/functions.php';
+require_once "$_tests_dir/includes/functions.php";
 
 function _manually_activate_plugin() {
 	Nzymes_Plugin::on_activation();
 
-	// Without the following call to WP_Roles->reinit(), my roles were right only when read from the database.
-	// If there was an empty database, like when starting tests from scratch, my nzymes.* capabilities were absent,
-	// even if they had been just added with the call above!
-
+	// Without the following call, my roles were right only when read from the database.
+	// If there was an empty database, like when starting tests from scratch, my nzymes.*
+    // capabilities were absent, even if they had been just added with the call above!
 	WP_Roles();
 }
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../nzymes.php';
+    global $_project_dir;
+	require "$_project_dir/nzymes.php";
 	add_action( 'init', '_manually_activate_plugin');
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-require $_tests_dir . '/includes/bootstrap.php';
-
+require "$_tests_dir/includes/bootstrap.php";
