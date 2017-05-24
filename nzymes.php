cf1e5807abb909dd136ba3f41d0c9ee4d8605dcc
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2017-05-24 10:29:07 +0200

    Add donate link

--- nzymes.php
+++ nzymes.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Nzymes
 Plugin URI: http://wordpress.org/plugins/nzymes/
-Description: Boost your blog with safe PHP injections. <a href="https://github.com/aercolino/nzymes/blob/master/nzymes-manual.md">Documentation</a>.
+Description: Boost your blog with safe PHP injections. <a href="https://github.com/aercolino/nzymes/blob/master/nzymes-manual.md">Documentation</a> | <a href="https://www.paypal.me/AndreaErcolino"><strong>Donate</strong></a>
 Version: 1.0.0
 Author: Andrea Ercolino
 Author URI: http://github.com/aercolino
