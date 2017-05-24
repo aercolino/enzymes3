e21a2f6b588bf4b1c257cb79cf7f8802f4ac86ad
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2017-05-24 10:30:06 +0200

    Fix license

--- nzymes.php
+++ nzymes.php
@@ -11,19 +11,20 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 /*
+Copyright 2017 Andrea Ercolino
 
-{Plugin Name} is free software: you can redistribute it and/or modify
+Nzymes is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
 
-{Plugin Name} is distributed in the hope that it will be useful,
+Nzymes is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with {Plugin Name}. If not, see {License URI}.
+along with Nzymes. If not, see http://www.gnu.org/licenses/gpl-2.0.html.
 */
 
 // Prohibit direct script loading.
