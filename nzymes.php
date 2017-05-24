99762a5f232d1ab0d161c17a634284830d12c2f8
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2017-05-24 10:29:37 +0200

    Split license from header

--- nzymes.php
+++ nzymes.php
@@ -8,6 +8,9 @@ Author: Andrea Ercolino
 Author URI: http://github.com/aercolino
 License: GPL2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/*
 
 {Plugin Name} is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
